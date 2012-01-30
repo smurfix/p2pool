b0965fba80efc314b21ef84e521be82d60c4de59
Matthias Urlichs <matthias@urlichs.de>
2012-01-30 20:19:23 +0100

    First take at Debianization
    no working init script yet

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import os
 import sys
 
-from distutils.core import setup
-import py2exe
+from distutils.core import setup,Extension
+try:
+	import py2exe
+except ImportError:
+	pass
+	
 
 def get_version():
     root_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
@@ -15,6 +19,18 @@ def get_version():
     else:
         return head[:7]
 
+# Debian build doesn't easily support multiple Python packages.
+# So we cheat.
+ext = {}
+if "DEB_BUILD_ARCH" in os.environ:
+	ltc_scrypt_module = Extension('ltc_scrypt',
+					sources = ['litecoin_scrypt/scryptmodule.c', 'litecoin_scrypt/scrypt.c'],
+					include_dirs=['litecoin_scrypt'])
+	ext["ext_modules"] = [ltc_scrypt_module]
+else:
+	# Debian packaging installs these itself
+    ext["data_files"]=[('', ['README', 'README-Litecoin'])],
+
 open('p2pool/__init__.py', 'wb').write('__version__ = %r\r\n\r\nDEBUG = False\r\n' % get_version())
 
 setup(name='p2pool',
@@ -23,7 +39,6 @@ setup(name='p2pool',
     author='Forrest Voight',
     author_email='forrest@forre.st',
     url='http://p2pool.forre.st/',
-    data_files=[('', ['README', 'README-Litecoin'])],
     
     console=['run_p2pool.py'],
     options=dict(py2exe=dict(
@@ -32,4 +47,7 @@ setup(name='p2pool',
         includes=['twisted.web.resource', 'ltc_scrypt'],
     )),
     zipfile=None,
+	packages=["p2pool","p2pool.bitcoin","p2pool.util","nattraverso"],
+	scripts=["run_p2pool.py"],
+	**ext
 )
